9509157ff4c9907f0ceec8db271fcc0fc3fcb83f
AYOUNES <amrhar.younes@berexia.com>
2019-07-27 19:23:01 +0100

    first commit

---
+++ src/main/java/packageappcommerce/Serveurgestionproduits1Application.java
@@ -0,0 +1,13 @@
+package packageappcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Serveurgestionproduits1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Serveurgestionproduits1Application.class, args);
+	}
+
+}
